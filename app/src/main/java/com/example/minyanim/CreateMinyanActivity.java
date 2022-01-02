c990402b22b21d04db11e992192bad12a875ae8d
shlomidaudi-ybamb <93072394+shlomidaudi-ybamb@users.noreply.github.com>
2022-01-02 23:20:59 +0200

    CreateMinyanActivity

---
+++ app/src/main/java/com/example/minyanim/CreateMinyanActivity.java
@@ -0,0 +1,20 @@
+package com.example.minyanim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateMinyanActivity extends AppCompatActivity {
+
+    // 1. choose location
+    // 2. choose time
+    // 3. upload to firebase
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_minyan);
+    }
+}
