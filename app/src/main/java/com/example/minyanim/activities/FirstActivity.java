8c36317bbb9072c29277d3c4eb053cce962e1f49
shlomidaudi-ybamb <93072394+shlomidaudi-ybamb@users.noreply.github.com>
2022-01-02 23:56:34 +0200

    FirstActivity

--- app/src/main/java/com/example/minyanim/activities/FirstActivity.java
+++ app/src/main/java/com/example/minyanim/activities/FirstActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.minyanim.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_first);
     }
 }
