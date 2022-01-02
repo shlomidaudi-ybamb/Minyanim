bd8ffae248aa47dc39239a4b448c53b0a534fe20
shlomidaudi-ybamb <93072394+shlomidaudi-ybamb@users.noreply.github.com>
2022-01-03 00:21:01 +0200

    LoginActivity

---
+++ app/src/main/java/com/example/minyanim/activities/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.minyanim.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.example.minyanim.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
